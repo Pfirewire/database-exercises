8a2f34963fbd6705bda3cfbde8135c4ef13e0426
Stephen Pflug <stephen@pflug.us>
2022-11-04 15:35:39 -0500

    changed data type in albums migration

--- data/albums_migration.sql
+++ data/albums_migration.sql
@@ -2,10 +2,10 @@ USE codeup_test_db;
     DROP TABLE IF EXISTS albums;
     CREATE TABLE albums (
         id INT UNSIGNED NOT NULL AUTO_INCREMENT,
-        artist VARCHAR(100),
-        name VARCHAR(100),
+        artist CHAR(100),
+        name CHAR(100),
         release_date INT,
         sales FLOAT,
-        genre VARCHAR(50),
+        genre CHAR(50),
         PRIMARY KEY (id)
     );
